fdae1cb8ef0548dd97061714e8d628607a25e067
junin <juninho.chandra@my.jcu.edu.au>
2026-06-20 22:58:13 +0800

    Connect and recoded viewmodel

--- app/src/main/java/au/edu/jcu/cp3406_cp5307_utilityappstartertemplate/CurrencyViewModel.kt
+++ app/src/main/java/au/edu/jcu/cp3406_cp5307_utilityappstartertemplate/CurrencyViewModel.kt
@@ -4,12 +4,31 @@ import androidx.lifecycle.ViewModel
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 
-class CurrencyViewModel : ViewModel() {
-    var result by mutableStateOf("")
+class CurrencyViewModel(
+    private val repository: CurrencyRepository
+) : ViewModel() {
+    var result by mutableStateOf("0.00")
         private set
 
-    fun updateResult(newResult: String) {
-        result = newResult
+    var isLoading by mutableStateOf(false)
+        private set
+
+    fun convert(amount: Double, from: String, to: String) {
+        viewModelScope.launch {
+            isLoading = true
+
+            val rates = repository.getRates()
+
+            val fromRate = rates.conversion_rates[from] ?: 1.0
+            val toRate = rates.conversion_rates[to] ?: 1.0
+
+            val converted = (amount / fromRate) * toRate
+
+            result = converted.toString()
+            isLoading = false
+        }
     }
 }
